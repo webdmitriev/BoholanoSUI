b8cd6b339bfc0a86d637a17aec22246861402d3b
webdmitriev <webdmitriev@gmail.com>
2025-07-31 15:43:06 +0300

    SingleGalleryDetailView is updated (dismiss)

--- BoholanoSUI/Presentation/Components/Single/SingleGalleryDetailView.swift
+++ BoholanoSUI/Presentation/Components/Single/SingleGalleryDetailView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct SingleGalleryDetailView: View {
     
+    @Environment(\.dismiss) private var dismiss
+    
     var gallery: [HousesGallery] = []
     let columns = [
         GridItem(.flexible(), spacing: 12),
@@ -17,17 +19,48 @@ struct SingleGalleryDetailView: View {
     let columnWidth = (UIScreen.main.bounds.width - 36) / 2
     
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            LazyVGrid(columns: columns, spacing: 12) {
-                ForEach(gallery, id: \.self) { img in
-                    NetworkImage(url: img.url)
-                        .frame(width: columnWidth, height: 150)
-                        .scaledToFill()
-                        .clipShape(RoundedRectangle(cornerRadius: 12))
+        ZStack(alignment: .topLeading) {
+            ScrollView(.vertical, showsIndicators: false) {
+                LazyVGrid(columns: columns, spacing: 12) {
+                    ForEach(gallery, id: \.self) { img in
+                        NetworkImage(url: img.url)
+                            .frame(width: columnWidth, height: 150)
+                            .scaledToFill()
+                            .clipShape(RoundedRectangle(cornerRadius: 12))
+                    }
                 }
+                .padding(.horizontal, 16)
             }
+            .padding(.top, 50)
+            
+            HStack {
+                Button(action: {
+                    dismiss()
+                }) {
+                    Image(systemName: "arrowshape.backward")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .foregroundStyle(.appBlack)
+                }
+                
+                Spacer()
+                
+                Text("Gallery")
+                    .multilineTextAlignment(.center)
+                    .font(.system(size: 24, weight: .bold, design: .default))
+                    .foregroundStyle(.appBlack)
+                
+                Spacer()
+                
+                Circle()
+                    .frame(width: 24, height: 24)
+                    .foregroundStyle(.clear)
+                    .background(.clear)
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.top, 5)
             .padding(.horizontal, 16)
         }
-        .navigationTitle(Text("Gallery"))
+        .navigationBarBackButtonHidden(true)
     }
 }
